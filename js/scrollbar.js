14161350d45b2016e4e13f966fede42cc7653509
artemphys <artemphys@gmail.com>
2015-03-27 18:39:56 +0300

    draggable&colorpicker

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,3 +1,9 @@
-/**
- * Created by Artem_Timoshenko on 3/27/2015.
- */
+(function( $ ) {
+    //$('.scroll-bar').click(function(){
+    //    $(this).append('<div id="draggable1" class="draggable">\
+    //    <div class="btn btn-xs input-group color-picker" >\
+    //    <span class="input-group-addon"><i>1</i></span></div></div>');
+    //});
+
+
+})(jQuery);
