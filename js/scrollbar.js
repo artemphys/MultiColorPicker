af9d23cb6768ae2c758ed06f125fe4b48a03d65a
artemphys <artemphys@gmail.com>
2015-03-30 11:56:22 +0300

    top&bottom points, position

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,13 +1,11 @@
 (function( $ ) {
-    //$('.scroll-bar').click(function(){
-    //    $(this).append('<div id="draggable1" class="draggable">\
-    //    <div class="btn btn-xs input-group color-picker" >\
-    //    <span class="input-group-addon"><i>1</i></span></div></div>');
-    //});
-
+            $('.scroll-bar').click(function () {
+                //$(this).append('<div id="draggable1" class="draggable">\
+                //<div class="btn btn-xs input-group color-picker" >\
+                //<span class="input-group-addon"><i>1</i></span></div></div>');
+            });
     $('.draggable').mousemove(function() {
-        var trianglePosition = $('.draggable').position().top;
-        $('.position-input').val(trianglePosition);
+        var trianglePosition = $(this).position().top;
+        $('.position-input', this).val(trianglePosition);
     });
-
 })(jQuery);
